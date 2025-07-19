f91397a21258c6316af091fe8522875d08105de3
Blade <Bladexpro@users.noreply.github.com>
2025-07-19 02:23:07 +0100

    V1 Gatherer

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ const colors = {
 };
 
 function randomCoords() {
-  return {
-    x: Math.random() * dimensions.width,
-    y: Math.random() * dimensions.height
+  return {// -100 to be used like padding
+    x: Math.random() * (dimensions.width-100),
+    y: Math.random() * (dimensions.height-100)
   };
 }
 
@@ -59,20 +59,31 @@ class Resource {
       drawSquare(this.x, this.y, dimensions.resource, colors.tree);
     }
   }
+
+  update(){
+    if(!this.alive){
+      this.alive = true;
+      this.targeted = false;
+      const { x, y } = randomCoords();
+      this.x = x;
+      this.y = y;
+    }
+  }
 }
 
 class Storage {
-  constructor(type = "wood") {
+  constructor(type = "wood", maxstorage = 10) {
     const {x, y} = randomCoords();
     this.x = x;
     this.y = y;
     this.type = type;
     this.stored = 0;
+    this.maxstorage = maxstorage;
   }
 
   draw() {
     drawSquare(this.x, this.y, dimensions.storage, colors.storage);
-    drawText(this.x, this.y - 10,`Logs: ${this.stored}`);
+    drawText(this.x, this.y - 10,`${this.type}: ${this.stored}`);
   }
 }
 
@@ -87,7 +98,6 @@ class House {
   draw() {
     drawSquare(this.x, this.y, dimensions.house, colors.storage);
     drawText(this.x + 5, this.y + dimensions.house / 2, "House");
-
   }
 }
 
@@ -96,16 +106,19 @@ class Npc {
     const {x, y} = randomCoords();
     this.x = x;
     this.y = y;
-    this.state = "walking";
+    this.targex = 0;
+    this.targey = 0;
+    this.state = "idle";//Ex:idle,working
     this.inventory = 0;
-    this.workingTime = 2; //in seconds or miliseconds
+    this.workingTime = 100;
+    this.workprogress = 0;
     this.type = type;
     this.name = name;
   }
 
   draw() {  
     context.drawImage(man, this.x, this.y, dimensions.npc, dimensions.npc)
-    drawText(this.x, this.y - 10, this.name);
+    drawText(this.x, this.y - 10, `${this.name} ${this.workprogress}`);
   }
 
     moveTo(x,y){
@@ -118,26 +131,111 @@ class Npc {
         else if (this.y > y)
             this.y = Math.max(this.y - dimensions.speed, y);
     }
+
+    isAt(x, y) {
+    return Math.abs(this.x - x) < dimensions.speed*2 && Math.abs(this.y - y) < dimensions.speed*2;
+    }
+
+
+    update(){
+        let targetStorage = undefined;
+        let targetResource = undefined;
+        switch(this.state) {
+          case 'idle':
+            targetStorage = storages.find(s => s.type === this.type && s.stored < s.maxstorage);
+            if(targetStorage){
+              targetResource = resources.find(r => r.type === this.type && r.alive && !r.targeted);
+              if (targetResource) {
+                //NEEDS to WORK to targetStorage
+                this.state = 'toResource';
+                // Find returns the first to pass the test resource that matches the type and is alive and not targeted
+                
+                // Move the npc toward the resource if found
+                if (targetResource) {
+                  targetResource.targeted = true;
+                  this.state = 'toResource'
+                  this.targex = targetResource.x;
+                  this.targey = targetResource.y;
+                  this.moveTo(this.targex, this.targey);
+                  }
+              }
+            }  
+            else
+              this.state = 'idle';
+              const targetHouse = houses.find(h => h.type === this.type);
+              if (targetHouse) {
+                this.moveTo(targetHouse.x, targetHouse.y);
+              }
+            break;
+          case 'toResource':
+            if(this.isAt(this.targex,this.targey))
+              this.state = 'working';
+            else
+              this.moveTo(this.targex, this.targey);
+            break;
+          case 'working':
+            if(this.workprogress < this.workingTime)
+              this.workprogress++;
+            else{
+              this.state = 'toStorage';
+              this.workprogress = 0;
+              targetResource = resources.find(r => r.type === this.type && r.alive && r.targeted && this.isAt(this.x,this.y));
+              if(!targetResource){
+                this.state = 'idle'
+                break;
+              }
+              targetResource.alive = false
+              targetStorage = storages.find(s => s.type === this.type && s.stored < s.maxstorage);
+              if(targetStorage){
+                this.inventory++
+                this.targex = targetStorage.x;
+                this.targey = targetStorage.y;
+              }
+              else
+                this.state = 'idle'
+            }
+            break;
+          case 'toStorage':
+            targetStorage = storages.find(s => s.type === this.type && s.stored < s.maxstorage);
+            if(targetStorage){
+              if(this.isAt(this.targex,this.targey)){
+                targetStorage.stored++
+                this.inventory--;
+                this.state = 'idle';
+              }
+              else{
+                this.moveTo(this.targex, this.targey);
+              }
+            }else{
+              this.inventory--;
+              this.state = 'idle';
+              }
+            
+            break;
+          default:
+            this.state = 'idle';
+        }
+    }
 }
 
 const man = new Image();
 
 const resources = [//Resource("type","name")
-  new Resource("wood","tree"),
-  new Resource("stone","stone"),
-  new Resource("food","berry")
+  new Resource("wood"),
+  new Resource("stone"),
+  new Resource("food")
 ];
 
 const npcs = [//Npc("type","name")
-  lumberjack = new Npc("wood","lumberjack"),
+  new Npc("wood","lumberjack"),
   new Npc("stone","miner"),
   new Npc("food","farmer")
 ];
 
-const storages = [//Storage("type")
-  new Storage("wood"),
-  new Storage("stone"),
-  new Storage("food")
+const storages = [//Storage("type",maxstorage)
+  new Storage("wood",5),
+  new Storage("stone",5),
+  new Storage("food",5)
 ];
 
 const houses = [//Storage("type")
@@ -147,28 +245,17 @@ const houses = [//Storage("type")
 ];
 
 function update(){
-for (let i = 0; i < npcs.length; i++) {
-    const npc = npcs[i];
-
-    // Find the first resource that matches the npc's type and is alive
-    const targetResource = resources.find(r => r.type === npc.type && r.alive);
-
-    // Move the npc toward the resource if found
-    if (targetResource) {
-      npc.moveTo(targetResource.x, targetResource.y);
-    }
+  for (let i = 0; i < npcs.length; i++) {
+    npcs[i].update()
   }
-
-  // Example: move lumberjack specifically to the wood house
-  const woodhouse = houses.find(h => h.type === "wood");
-  if (woodhouse) {
-    lumberjack.moveTo(woodhouse.x, woodhouse.y);
+    for (let i = 0; i < resources.length; i++) {
+    resources[i].update()
   }
 }
 
 function draw(){
-    for (let i = 0; i < resources.length; i++) {
-    resources[i].draw();
+    for (let i = 0; i < houses.length; i++) {
+    houses[i].draw();
     }
     for (let i = 0; i < npcs.length; i++) {
     npcs[i].draw();
@@ -176,8 +263,8 @@ function draw(){
     for (let i = 0; i < storages.length; i++) {
     storages[i].draw();
     }
-    for (let i = 0; i < houses.length; i++) {
-    houses[i].draw();
+    for (let i = 0; i < resources.length; i++) {
+    resources[i].draw();
     }
 }
 
@@ -189,10 +276,9 @@ function init() {
 
 function newframe() {
     context.clearRect(0, 0, canvas.width, canvas.height); // clear before redraw
-    update()   
+    update()
     draw()
     window.requestAnimationFrame(newframe);
 }
-
 
 init()
